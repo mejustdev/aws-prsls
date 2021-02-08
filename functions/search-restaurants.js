6a61a2cd88aff73ce96b2234b1c32cd9eecbbbfc
Me <mergorgec@gmail.com>
2021-02-08 15:45:58 +0100

    add Using a simple logger

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,13 +1,18 @@
-const middy = require('@middy/core');
-const ssm = require('@middy/ssm');
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient;
 const dynamodb = new DocumentClient();
+const middy = require('@middy/core');
+const ssm = require('@middy/ssm');
+const Log = require('@dazn/lambda-powertools-logger');
 
 const { serviceName, stage } = process.env;
 const tableName = process.env.restaurants_table;
 
 const findRestaurantsByTheme = async (theme, count) => {
-  console.log(`finding (up to ${count}) restaurants with the theme ${theme}...`);
+  Log.debug('finding restaurants with theme...', {
+    count,
+    theme,
+    tableName,
+  });
   const req = {
     TableName: tableName,
     Limit: count,
@@ -16,11 +21,15 @@ const findRestaurantsByTheme = async (theme, count) => {
   };
 
   const resp = await dynamodb.scan(req).promise();
-  console.log(`found ${resp.Items.length} restaurants`);
+  Log.debug('found restaurants', {
+    count: resp.Items.length,
+  });
   return resp.Items;
 };
 
 module.exports.handler = middy(async (event, context) => {
+  console.info(context.secretString);
+
   const req = JSON.parse(event.body);
   const theme = req.theme;
   const restaurants = await findRestaurantsByTheme(theme, process.env.defaultResults);
@@ -28,7 +37,7 @@ module.exports.handler = middy(async (event, context) => {
     statusCode: 200,
     body: JSON.stringify(restaurants),
   };
-  console.info(context.secretString);
+
   return response;
 })
   .use(
